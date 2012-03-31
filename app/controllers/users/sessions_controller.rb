7d7002fdeb2e52affe6d3cb7c98b0e8284cdd59f
parasquid <tristan.gomezd@gmail.com>
2012-04-01 03:15:43 +0800

    fix user signed in bug

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   def new
-    unless current_user.signed_in?
+    unless user_signed_in?
       redirect_to omniauth_authorize_path(resource_name, :facebook)
     end
   end
