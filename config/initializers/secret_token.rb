1968b28e196162993fee178639deb1dca2448c5d
parasquid <tristan.gomezd@gmail.com>
2012-03-31 14:50:15 +0800

    rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TraqTastic::Application.config.secret_token = 'b67d16dcae4b1c263af14f771fea1aa9625701c9a66bc8deb49d898b340c14e3c5ea31c9e00d145b6b5af9c2212618fe1324f9276209e50ed3ef618857ed41e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TraqTastic::Application.config.session_store :cookie_store, key: '_TraqTastic_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TraqTastic::Application.config.session_store :active_record_store
